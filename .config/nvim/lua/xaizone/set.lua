1fb5a004f63381c3f65d5a77de030a989ee83cef
xaizone <git@xaizone.eu>
2024-05-20 22:02:38 +0200

    neovim cleanup

--- .config/nvim/lua/xaizone/set.lua
+++ .config/nvim/lua/xaizone/set.lua
@@ -14,14 +14,13 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.cmd 'colorscheme vim'
-vim.opt.termguicolors = false
+vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.isfname:append("@-@")
